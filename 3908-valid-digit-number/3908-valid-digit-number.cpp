4d4b32dcba9f5f49c3873a0b190813908b759f5c
Pranav gupta <pranavgupta080506@gmail.com>
2026-08-10 00:26:10 +0530

    Time: 0 ms (100%), Space: 7.9 MB (83.03%) - LeetHub

---
+++ 3908-valid-digit-number/3908-valid-digit-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool validDigit(int n, int x) {
+        bool found = false;
+        while(n>9){
+            if(n%10==x)
+                found=true;
+                n/=10;
+
+            
+            
+        }
+        return found && n!=x;
+
+
+    }
+};
